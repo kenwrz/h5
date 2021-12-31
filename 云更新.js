d8b1653916446fbf1df35e7da3ecffa19be63f4c
kenwrz <82634265+kenwrz@users.noreply.github.com>
2022-01-01 02:34:45 +0800

    Update 云更新.js

--- 云更新.js
+++ 云更新.js
@@ -15,30 +15,27 @@ ui.gx_list.click(function(){
         threads.start(function() {
         //这里写你的流程代码;
         //gx_txt用于接收返回的更新列表内容,然后根据内容下载完成更新;
-        // let gx_tq_txt=x_z_jb(js_name);
-        // const mm=gx_tq_txt.match(/<spa.*<\/s[a-z]{3}>/)[0];
-        // log(mm)
-        var gx_txt="云更新";
-        //log(gx_txt);
-        //文件可以用读入一行,这个是文本需要分割成数组;
-        name_js_arr=new Array();
-        //分割文本到数组;
-        name_js_arr=gx_txt.split(",");
-        //1个以上的文件名,就可以更新了;
-        //以下是更新下载;
-        if(name_js_arr.length>0){
-            for(i=0;i<name_js_arr.length;i++){
-                //下载并生成文件到本地;
-                b_c_wj(x_z_jb(name_js_arr[i]),name_js_arr[i]);
-            }
-        }else{toastLog("没有需要更新的文件")};
-        toastLog("全部更新完成");
-
-        
+        let gx_tq_txt=x_z_jb(js_name);
+        if(gx_tq_txt=="none"){
+            toastLog("暂无更新");
+        }else{
+            name_js_arr=new Array();
+            //分割文本到数组;
+            name_js_arr=gx_tq_txt.split(",");
+            //1个以上的文件名,就可以更新了;
+            //以下是更新下载;
+            if(name_js_arr.length>0){
+                for(i=0;i<name_js_arr.length;i++){
+                    //下载并生成文件到本地;
+                    b_c_wj(x_z_jb(name_js_arr[i]),name_js_arr[i]);
+                };
+            }else{toastLog("没有需要更新的文件")};
+            toastLog("全部更新完成");
+        };   
     });
 });   
 
-//下载打开远程文件;
+//下载打开远程文件,返回处理好的文件内容(仅提取内容)
 function x_z_jb (js_name){
     
     var url="https://github.com/kenwrz/h5/blob/gh-pages/"+js_name+".js";
@@ -50,7 +47,7 @@ function x_z_jb (js_name){
         }
     });
     var html_r=r.body.string();
-    //log(html_r)
+    log(html_r)
     if(r.statusCode==200){
         //提取网页内容后,交给其它需要的程序处理;
         //提取字符串之前的所有字符
@@ -59,10 +56,6 @@ function x_z_jb (js_name){
         //提取字符串之后的所有字符
         index = html_r.lastIndexOf("<table");
         html_r =html_r.substring(index+1,html_r.length);
-
-        //清除空格回车等
-        // html_r=html_r.replace(/\s*/g,"")       
-        // html_r = html_r.replace(/[\r\n]/g,"");
         return t_q_wb(html_r);
         
     }else{return "none"};
@@ -82,48 +75,43 @@ function t_q_wb (html_r){
         var str_txt_all="";
         const re_LC=/LC/g;
         var LC_cisu_arr =html_r.match(re_LC);
-        if(LC_cisu_arr==null){log("提取失败");exit();}else{log("内容行数:",LC_cisu_arr.length);};
-        for(ci=1;ci<LC_cisu_arr.length+1;ci++){
-            let lc_str="LC"+ci;
-            let index_lc= html_r.indexOf(lc_str);
-            let index_tr= html_r.indexOf("</tr>")+6
-            var str_txt=html_r.substring(index_lc,index_tr);
-            html_r =html_r.substring(index_tr+1,html_r.length);
-            //log(str_txt);
-            //有注释直接跳过;
-            if(str_txt.indexOf("//")!=-1){continue;};
-            if(str_txt.match(/<spa.*<\/s[a-z]{3}>/)!=null){
-                str_txt = str_txt.match(/<spa.*<\/s[a-z]{3}>/)[0];}else{continue;};
-            //log(str_txt);
-            //str_txt = str_txt.replace(/LC.*?-line">/g, '');
-            str_txt = str_txt.replace(/<spa.*?>/g, '');
-            str_txt = str_txt.replace(/<\/s[a-z]{3}>/g, '');
-            //str_txt = str_txt.replace(/<\/td>/, '\n');
-            //str_txt = str_txt.replace(/<\/tr>/, '');
-            str_txt = str_txt.replace(/&lt;/g, '<');
-            str_txt = str_txt.replace(/&gt;/g, '>');
-            str_txt = str_txt.replace(/&quot;/g, '"');
-            str_txt = str_txt.replace(/&#39;/g, "'");
-            str_txt = str_txt.replace(/&amp;/g, '&');
-            //str_txt = str_txt.replace(/&nbsp;/g, '');
-            //清右边空白
-            str_txt =str_txt.replace(/(\s*$)/g,"");
-            //str_txt=str_txt.substring(1,str_txt.indexOf("\n")+2)
-            log("==>",str_txt);
-            if(str_txt_all==""){
-                str_txt_all=str_txt;
-            }else{
-                str_txt_all=str_txt_all+str_txt;};
-            
-                
-        }
-        //str_txt_all=str_txt_all
-        //log("总"+ci+"===>",str_txt_all);
-        return str_txt_all+"\r\n";
+        if(LC_cisu_arr==null){toastLog("没有更新文件");return "none";
+        }else{
+                //log("内容行数:",LC_cisu_arr.length);
+                for(ci=1;ci<LC_cisu_arr.length+1;ci++){
+                    let lc_str="LC"+ci;
+                    let index_lc= html_r.indexOf(lc_str);
+                    let index_tr= html_r.indexOf("</tr>")+6
+                    var str_txt=html_r.substring(index_lc,index_tr);
+                    html_r =html_r.substring(index_tr+1,html_r.length);
+                    //有注释直接跳过;
+                    if(str_txt.indexOf("//")!=-1){continue;};
+                    if(str_txt.match(/<spa.*<\/s[a-z]{3}>/)!=null){
+                        str_txt = str_txt.match(/<spa.*<\/s[a-z]{3}>/)[0];}else{continue;};
+                    //log(str_txt);
+                    //str_txt = str_txt.replace(/LC.*?-line">/g, '');
+                    str_txt = str_txt.replace(/<spa.*?>/g, '');
+                    str_txt = str_txt.replace(/<\/s[a-z]{3}>/g, '');
+                    str_txt = str_txt.replace(/&lt;/g, '<');
+                    str_txt = str_txt.replace(/&gt;/g, '>');
+                    str_txt = str_txt.replace(/&quot;/g, '"');
+                    str_txt = str_txt.replace(/&#39;/g, "'");
+                    str_txt = str_txt.replace(/&amp;/g, '&');
+                    //str_txt = str_txt.replace(/&nbsp;/g, '');
+                    //清右边空白
+                    str_txt =str_txt.replace(/(\s*$)/g,"");
+                    log("==>",str_txt);
+                    if(str_txt_all==""){
+                        str_txt_all=str_txt;
+                    }else{
+                        str_txt_all=str_txt_all+str_txt;};        
+                };
+                log("总"+ci+"===>",str_txt_all);
+                return str_txt_all;
+            };
+        
     }else{return "none"};
-
-}
-
+};
 //将代码写出到脚本当前目录以便调用;
 function b_c_wj (html_r,t_name){
     if(html_r!="none"){
